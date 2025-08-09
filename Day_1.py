d8ea3769b2746a0d2c96c8b01cd35c8afbafe45f
nitinpradhan <89532673+nitinpradhan@users.noreply.github.com>
2025-08-09 16:51:32 +0530

    Delete Day_1.py

--- Day_1.py
+++
@@ -1,7 +0,0 @@
-print("hello world  ")
-#name=input("Enter you name:")
-#print("hello", name)
-import random
-print("Random number:", random.randint(1, 100))
-n = random.randint(2,5)
-print ("random no:" , n)
